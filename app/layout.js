f4948bee815a0f1338b6bfda91d032cbf381e1cd
neelthepatel <patel.neel5@northeastern.edu>
2024-09-14 20:57:18 -0400

    first commit

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "tunedown | Harmonize your Heartbeat",
+  description: "Harmonize your Heartbeat",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
